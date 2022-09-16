15a1a72f50ccaa5879fd72891168beda35fd230d
fahrulamirudin <58175401+fahrulamirudin@users.noreply.github.com>
2022-09-16 21:12:49 +0700

    TUGAS KE 2

--- promise.js
+++ promise.js
@@ -1,7 +1,16 @@
-const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
+const {
+  promiseTheaterIXX,
+  promiseTheaterVGC
+} = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
+const promiseOutput = async (emosi) => {
+  const ixx = await promiseTheaterIXX(emosi);
+  const vgc = await promiseTheaterVGC(emosi);
+  const DataneDigabung= ixx.concat(vgc);
+  const hasile = DataneDigabung.filter(data => data.hasil === emosi);
+  return hasile.length;
+};
 
 module.exports = {
   promiseOutput,
